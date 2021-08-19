3406187e71a41693255d0519b803646287501492
Edgar Andres Bonilla Grajales <edgar.a.bonilla.g@gmail.com>
2021-08-18 19:19:24 -0500

    Added custom icons, behavior and colors to the main tab bar

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import { RouteProp } from '@react-navigation/native';
 const { createBottomTabNavigator } = require('@react-navigation/bottom-tabs');
 
 import { HistoryScreen, HomeScreen } from '../screens';
 
+import { colors } from '../utils/theme';
+
+type Route = RouteProp<Record<string, object | undefined>, string>;
+
 const Tab = createBottomTabNavigator();
+
+const getIconName = (routeName: string) => {
+  let iconName = '';
+  switch (routeName) {
+    case 'HomeTab':
+      iconName = 'home';
+      break;
+    case 'HistoryTab':
+      iconName = 'history';
+      break;
+    default:
+      iconName = 'help-outline';
+      break;
+  }
+
+  return iconName;
+};
+
+const navigatorScreenOptions = ({ route }: { route: Route }) => ({
+  tabBarIcon: ({ focused, color, size }: { focused: boolean; color: string; size: number }) => {
+    const iconName = getIconName(route.name);
+    const iconSize = focused ? size * 1.2 : size;
+    return <MaterialIcon name={iconName} size={iconSize} color={color} />;
+  },
+  tabBarAllowFontScaling: false,
+  tabBarActiveTintColor: colors.mainOrange,
+  tabBarInactiveTintColor: colors.lightBlue,
+  tabBarLabelStyle: {
+    fontSize: 12,
+  },
+});
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="HomeTab" component={HomeScreen} />
-      <Tab.Screen name="HistoryTab" component={HistoryScreen} />
+    <Tab.Navigator screenOptions={navigatorScreenOptions}>
+      <Tab.Screen name="HomeTab" component={HomeScreen} options={{ title: 'Home' }} />
+      <Tab.Screen name="HistoryTab" component={HistoryScreen} options={{ title: 'History' }} />
     </Tab.Navigator>
   );
 };
