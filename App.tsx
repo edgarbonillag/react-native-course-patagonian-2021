a8330eabf1bd0f63f30585a40b6223d80ee50073
Edgar Andres Bonilla Grajales <edgar.a.bonilla.g@gmail.com>
2021-08-23 19:29:30 -0500

    Integrated 'react-native-config' library to be able to work with .env files

--- App.tsx
+++ App.tsx
@@ -4,7 +4,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './src/navigation/MainNavigator';
 import { navigationRef } from './src/navigation/controls';
 
+import { API_URL } from './src/config/envVariables';
+
 const App = () => {
+  console.log('API_URL', API_URL);
   return (
     <NavigationContainer ref={navigationRef}>
       <MainNavigator />
